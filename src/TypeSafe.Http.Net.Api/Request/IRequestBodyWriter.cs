e06bfa125d84009fbe7e621509f41e2c146a6df9
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 17:16:06 -0500

    Added an interface for request body writers

---
+++ src/TypeSafe.Http.Net.Api/Request/IRequestBodyWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Contract for types that can write body data.
+	/// </summary>
+	public interface IRequestBodyWriter
+	{
+		/// <summary>
+		/// Writes binary bytes as the content of the request body.
+		/// </summary>
+		/// <param name="bytes">The bytes to write.</param>
+		void Write(byte[] bytes);
+
+		/// <summary>
+		/// Writes text/string data as the content of the request body.
+		/// </summary>
+		/// <param name="content"></param>
+		void Write(string content);
+	}
+}
