68ab6d6fdc42a9672c77611570ffee28427d889a
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 17:15:54 -0500

    Added interface for a request header object

---
+++ src/TypeSafe.Http.Net.Api/Request/IRequestHeader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	public interface IRequestHeader
+	{
+		/// <summary>
+		/// Indicates the header type.
+		/// </summary>
+		string HeaderType { get; }
+
+		/// <summary>
+		/// The fully built header value containing one or more
+		/// values with the correct delimiters.
+		/// </summary>
+		string HeaderValue { get; }
+	}
+}
