ce02fbe229ee3dfe1f068dfa993a3a842c7d09bc
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 18:39:45 -0500

    Added action path builder service

---
+++ src/TypeSafe.Http.Net.Api/Middleware/IActionPathServiceCallInterpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Service that interprets service calls to produce <see cref="string"/> action paths.
+	/// These are the paths appended to the
+	/// </summary>
+	public interface IActionPathServiceCallInterpreter : IRequestPipelineService<string>
+	{
+		
+	}
+}
