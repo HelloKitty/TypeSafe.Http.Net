583be8c86528a86f6e689eab9914f6e00abd9a22
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 17:15:39 -0500

    Added rest service proxy interface

---
+++ src/TypeSafe.Http.Net.Api/Service/IRestServiceProxy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Contract for rest service proxies that mediate between the HTTP application layer and the consuming user.
+	/// </summary>
+	/// <typeparam name="TProxyInterface">The interface proxy type.</typeparam>
+	public interface IRestServiceProxy<TProxyInterface> : IRestService
+		where TProxyInterface : class //really we should be constraining to interface but that's not possible.
+	{
+		Task<TReturnType> Send<TReturnType>(IRestClientRequestContext requestContext);
+	}
+}
