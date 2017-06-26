4b6f199b8c8b6735d2c752511c266a5da3a3f553
HelloKitty <andrew.blakely@ymail.com>
2017-06-25 21:48:16 -0500

    Added HttpBaseMethodAttribute
    This is the base attribute for all REST attributes that take a path and
    an HTTP method like GET or POST.

---
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Methods/HttpBaseMethodAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Base metadata type for all HTTP method types.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+	public abstract class HttpBaseMethodAttribute : Attribute
+	{
+		/// <summary>
+		/// Indicates the HTTP method used.
+		/// </summary>
+		public HttpMethod Method { get; }
+
+		/// <summary>
+		/// The endpoint path. This should not include the base path.
+		/// (Ex. /api/auth)
+		/// </summary>
+		public string Path { get; }
+
+		protected HttpBaseMethodAttribute(string path, HttpMethod method)
+		{
+			if (method == null) throw new ArgumentNullException(nameof(method));
+			if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException($"The path for the {GetType().FullName} attribute must not be null or empty.", nameof(path));
+
+			Path = path;
+			Method = method;
+		}
+	}
+}
