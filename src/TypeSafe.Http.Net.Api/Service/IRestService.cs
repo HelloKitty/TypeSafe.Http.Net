4ea4a9a3fbc973eeab86f6a2b52749a3c04fd089
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 17:15:24 -0500

    Added base IRestService interface

---
+++ src/TypeSafe.Http.Net.Api/Service/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Contract for all rest services.
+	/// </summary>
+	public interface IRestService
+	{
+		/// <summary>
+		/// The base endpoint of the REST service.
+		/// </summary>
+		string BaseUrl { get; }
+	}
+}
