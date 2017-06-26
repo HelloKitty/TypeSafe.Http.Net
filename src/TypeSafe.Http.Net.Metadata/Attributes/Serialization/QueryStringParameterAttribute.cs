e02c457b6cf9cb71c77155c1cb2c9da01ad16bc3
HelloKitty <andrew.blakely@ymail.com>
2017-06-26 17:14:40 -0500

    Added QueryStringParameter attribute

---
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Serialization/QueryStringParameterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Similar to ASP Core's QueryString attribut that indicates a parameter is coming from a query string this metadata
+	/// marker will indicate that a parameter should be inserted into the the querystring.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Parameter)]
+	public class QueryStringParameterAttribute : Attribute
+	{
+		//If you want to modify the name you should use the Alias attribute.
+		//The functionality for binding the parameter to a specified named value
+		//is not controlled exclusively by this attribute.
+	}
+}
