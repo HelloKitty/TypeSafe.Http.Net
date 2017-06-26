39a4191235720871e9f47c6f8abea4cd11dd4895
HelloKitty <andrew.blakely@ymail.com>
2017-06-25 21:48:24 -0500

    Added DeleteAttribute

---
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Methods/DeleteAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using TypeSafe.Http.Net;
+
+namespace TypeSafe.HTTP.NET.Metadata.Attributes
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class DeleteAttribute : HttpBaseMethodAttribute
+	{
+		/// <inheritdoc />
+		public DeleteAttribute(string path)
+			: base(path, HttpMethod.Delete)
+		{
+
+		}
+	}
+}
