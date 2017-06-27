4a06269ddd368aa1108adb308e8131b2395ca778
HelloKitty <andrew.blakely@ymail.com>
2017-06-27 15:42:27 -0500

    Added ResponseContentAttribute

---
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Serialization/ResponseContentAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Base attribute that controls how a body is serialized.
+	/// You should inherit this attribute to enable serialization.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.ReturnValue)]
+	public abstract class ResponseContentAttribute : Attribute
+	{
+		protected ResponseContentAttribute()
+		{
+
+		}
+	}
+}
