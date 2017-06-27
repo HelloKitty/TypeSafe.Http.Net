a21cadcd5ff9e65a3e93c56bacaf993316ed34d0
HelloKitty <andrew.blakely@ymail.com>
2017-06-27 13:46:39 -0500

    Added HttpRequestWriter and ResponseReader

---
+++ src/TypeSafe.Http.Net.HttpClient/Message/HttpClientRequestBodyWriter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// <see cref="HttpClient"/> implementation of the request body writer.
+	/// </summary>
+	public sealed class HttpClientRequestBodyWriter : IRequestBodyWriter
+	{
+		public HttpRequestMessage RequestMessage { get; }
+
+		public HttpClientRequestBodyWriter(HttpRequestMessage requestMessage)
+		{
+			if (requestMessage == null) throw new ArgumentNullException(nameof(requestMessage));
+
+			RequestMessage = requestMessage;
+		}
+
+		/// <inheritdoc />
+		public void Write(byte[] bytes)
+		{
+			//TODO: Should we worry about the content type it might set?
+			RequestMessage.Content = new ByteArrayContent(bytes);
+			RequestMessage.Content.Headers.Clear();
+		}
+
+		/// <inheritdoc />
+		public void Write(string content)
+		{
+			RequestMessage.Content = new StringContent(content);
+			RequestMessage.Content.Headers.Clear();
+		}
+
+		/// <inheritdoc />
+		public Task WriteAsync(byte[] bytes)
+		{
+			//TODO: Does this capture exceptions still?
+			//TODO: Should we worry about the content type it might set?
+			RequestMessage.Content = new ByteArrayContent(bytes);
+			RequestMessage.Content.Headers.Clear();
+
+			return Task.CompletedTask;
+		}
+
+		/// <inheritdoc />
+		public Task WriteAsync(string content)
+		{
+			//TODO: Does this capture exceptions still?
+			RequestMessage.Content = new StringContent(content);
+			RequestMessage.Content.Headers.Clear();
+
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ src/TypeSafe.Http.Net.HttpClient/Message/HttpClientResponseBodyReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeSafe.Http.Net
+{
+	public sealed class HttpClientResponseBodyReader : IResponseBodyReader
+	{
+		/// <summary>
+		/// The HTTP response message.
+		/// </summary>
+		private HttpResponseMessage Response { get; }
+
+		public HttpClientResponseBodyReader(HttpResponseMessage response)
+		{
+			if (response == null) throw new ArgumentNullException(nameof(response));
+
+			Response = response;
+		}
+
+		/// <inheritdoc />
+		public async Task<byte[]> ReadAsBytesAsync()
+		{
+			return await Response.Content.ReadAsByteArrayAsync();
+		}
+
+		/// <inheritdoc />
+		public async Task<string> ReadAsStringAsync()
+		{
+			return await Response.Content.ReadAsStringAsync();
+		}
+
+		/// <inheritdoc />
+		public byte[] ReadAsBytes()
+		{
+			return Response.Content.ReadAsByteArrayAsync().Result;
+		}
+
+		/// <inheritdoc />
+		public string ReadAsString()
+		{
+			return Response.Content.ReadAsStringAsync().Result;
+		}
+	}
+}
