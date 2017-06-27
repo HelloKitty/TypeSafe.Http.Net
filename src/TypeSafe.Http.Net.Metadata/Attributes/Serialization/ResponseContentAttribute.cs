57cae1611d676319d17ab337894433c4c7caa283
HelloKitty <andrew.blakely@ymail.com>
2017-06-27 15:45:30 -0500

    Updated ResponseContentAttribute doc

--- src/TypeSafe.Http.Net.Metadata/Attributes/Serialization/ResponseContentAttribute.cs
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Serialization/ResponseContentAttribute.cs
@@ -5,8 +5,8 @@ using System.Text;
 namespace TypeSafe.Http.Net
 {
 	/// <summary>
-	/// Base attribute that controls how a body is serialized.
-	/// You should inherit this attribute to enable serialization.
+	/// Base attribute that controls how a return value can be deserialized
+	/// You should inherit this attribute to enable return value serialization.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.ReturnValue)]
 	public abstract class ResponseContentAttribute : Attribute
