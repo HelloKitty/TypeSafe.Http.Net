047169df7d6e608aad454faf54ae075c88f1200e
HelloKitty <andrew.blakely@ymail.com>
2017-06-27 13:46:26 -0500

    Extended IRestServiceProxy to allow no return value

--- src/TypeSafe.Http.Net.Api/Service/IRestServiceProxy.cs
+++ src/TypeSafe.Http.Net.Api/Service/IRestServiceProxy.cs
@@ -12,6 +12,21 @@ namespace TypeSafe.Http.Net
 	public interface IRestServiceProxy<TProxyInterface> : IRestService
 		where TProxyInterface : class //really we should be constraining to interface but that's not possible.
 	{
-		Task<TReturnType> Send<TReturnType>(IRestClientRequestContext requestContext);
+		/// <summary>
+		/// Sends a request with the designated <see cref="TReturnType"/> with the provided context and the strategy
+		/// for deserializing the response.
+		/// </summary>
+		/// <typeparam name="TReturnType">The return type to expect.</typeparam>
+		/// <param name="requestContext">The context of the request.</param>
+		/// <param name="deserializer">The deserialization strategy.</param>
+		/// <returns>The a future promise for a deserialized return data.</returns>
+		Task<TReturnType> Send<TReturnType>(IRestClientRequestContext requestContext, IResponseDeserializationStrategy<TReturnType> deserializer);
+
+		/// <summary>
+		/// Sends a request with the provided context with no response data.
+		/// </summary>
+		/// <param name="requestContext">The context of the request.</param>
+		/// <returns>A future.</returns>
+		Task Send(IRestClientRequestContext requestContext);
 	}
 }
