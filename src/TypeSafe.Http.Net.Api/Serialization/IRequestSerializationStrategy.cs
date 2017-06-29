8bb97c34ade802463064a725335dc06ad99919fa
HelloKitty <andrew.blakely@ymail.com>
2017-06-29 16:36:27 -0500

    Added Serialization Strategy

---
+++ src/TypeSafe.Http.Net.Api/Serialization/IRequestSerializationStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeSafe.Http.Net
+{
+	//This may seem like there is a lot of indirection here but we need to make it this way because
+	//both the method in which the content is put into a request AND the way in which the content is serialized must be abstracted from eachother AND from
+	//the core functions itself to be pluggable replacements for serializers and HTTP clients.
+	/// <summary>
+	/// Contract for types that provide serialization services.
+	/// </summary>
+	public interface IRequestSerializationStrategy
+	{
+		/// <summary>
+		/// Tries to serialize the content
+		/// </summary>
+		/// <typeparam name="TContentType"></typeparam>
+		/// <param name="content">Content to try to serialize into the writer.</param>
+		/// <param name="writer">The writer to write the serialized content into.</param>
+		/// <returns></returns>
+		bool TrySerialize<TContentType>(TContentType content, IRequestBodyWriter writer);
+	}
+}
--- src/TypeSafe.Http.Net.Api/Serialization/IResponseDeserializationStrategy.cs
+++ src/TypeSafe.Http.Net.Api/Serialization/IResponseDeserializationStrategy.cs
@@ -6,24 +6,23 @@ using System.Threading.Tasks;
 namespace TypeSafe.Http.Net
 {
 	/// <summary>
-	/// Strategy for deserializing an instance of <typeparamref name="TReturnType"/> from
+	/// Strategy for deserializing responses from
 	/// a provided <see cref="IResponseBodyReader"/>.
 	/// </summary>
-	/// <typeparam name="TReturnType"></typeparam>
-	public interface IResponseDeserializationStrategy<TReturnType>
+	public interface IResponseDeserializationStrategy
 	{
 		/// <summary>
 		/// Deserializes the response from the provided response body reader.
 		/// </summary>
 		/// <param name="reader">A reader service that can interpret the response.</param>
 		/// <returns>A non-null <typeparamref name="TReturnType"/> deserialized from the body.</returns>
-		TReturnType Deserialize(IResponseBodyReader reader);
+		TReturnType Deserialize<TReturnType>(IResponseBodyReader reader);
 
 		/// <summary>
 		/// Deserializes the response asyncronously from the provided response body reader.
 		/// </summary>
 		/// <param name="reader">A reader service that can interpret the response.</param>
 		/// <returns>A non-null <typeparamref name="TReturnType"/> deserialized from the body.</returns>
-		Task<TReturnType> DeserializeAsync(IResponseBodyReader reader);
+		Task<TReturnType> DeserializeAsync<TReturnType>(IResponseBodyReader reader);
 	}
 }
