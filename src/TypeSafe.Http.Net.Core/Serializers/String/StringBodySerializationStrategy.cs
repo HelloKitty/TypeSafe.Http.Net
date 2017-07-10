46162055c39b935a99275ef6fc9379454d846be7
HelloKitty <andrew.blakely@ymail.com>
2017-07-10 03:25:14 -0500

    Added Basic String Body Content Serializer

---
+++ src/TypeSafe.Http.Net.Core/Serializers/String/StringBodySerializationStrategy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeSafe.Http.Net
+{
+	public sealed class StringBodySerializationStrategy : IRequestSerializationStrategy, IResponseDeserializationStrategy, IContentTypeAssociable
+	{
+		/// <inheritdoc />
+		public IEnumerable<string> AssociatedContentType { get; }
+
+		public StringBodySerializationStrategy()
+		{
+			//The content type for Url encoded bodies is almost always ONLY that.
+			AssociatedContentType = new string[] { @"text/plain" };
+		}
+
+		/// <inheritdoc />
+		public bool TrySerialize(object content, IRequestBodyWriter writer)
+		{
+			if (content == null) throw new ArgumentNullException(nameof(content));
+
+			writer.Write(content.ToString(), AssociatedContentType.First());
+
+			return true;
+		}
+
+		/// <inheritdoc />
+		public TReturnType Deserialize<TReturnType>(IResponseBodyReader reader)
+		{
+			//This is ugly but we should TRUST that the caller knows what they are doing
+			//but it is very likely that the TReturnType is a string
+			return (TReturnType)(object)reader.ReadAsString();
+		}
+
+		/// <inheritdoc />
+		public async Task<TReturnType> DeserializeAsync<TReturnType>(IResponseBodyReader reader)
+		{
+			//This is ugly but we should TRUST that the caller knows what they are doing
+			//but it is very likely that the TReturnType is a string
+			return (TReturnType)(object)await reader.ReadAsStringAsync();
+		}
+	}
+}
---
+++ src/TypeSafe.Http.Net.Metadata/Attributes/Serialization/Concrete/StringBodyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Metadata marker that indicates an object should be serialized in the body
+	/// using basic string content body.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Parameter)]
+	public sealed class StringBodyAttribute : BodyContentAttribute
+	{
+		public StringBodyAttribute()
+			: base()
+		{
+
+		}
+	}
+}
