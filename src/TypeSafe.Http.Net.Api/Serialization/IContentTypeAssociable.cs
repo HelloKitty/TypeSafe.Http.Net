3545049f471803e28ab21c5232fa5b6669df1bd7
HelloKitty <andrew.blakely@ymail.com>
2017-07-09 19:24:43 -0500

    Added IContentTypeAssociable

---
+++ src/TypeSafe.Http.Net.Api/Serialization/IContentTypeAssociable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Contract for types that are associated with a 0 to n
+	/// many content types.
+	/// </summary>
+	public interface IContentTypeAssociable
+	{
+		/// <summary>
+		/// The associated types.
+		/// Possibly empty but never null.
+		/// </summary>
+		IEnumerable<string> AssociatedContentType { get; }
+	}
+}
