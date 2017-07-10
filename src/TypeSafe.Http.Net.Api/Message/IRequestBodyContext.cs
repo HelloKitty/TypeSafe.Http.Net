5684466baa068780faa949197e692d08e08cae0a
HelloKitty <andrew.blakely@ymail.com>
2017-07-09 19:24:02 -0500

    Aded IRequestBodyContext

---
+++ src/TypeSafe.Http.Net.Api/Message/IRequestBodyContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	/// <summary>
+	/// Context for the body of the request.
+	/// </summary>
+	public interface IRequestBodyContext
+	{
+		/// <summary>
+		/// Indicates if the request contains a body to write.
+		/// </summary>
+		bool HasBody { get; }
+
+		/// <summary>
+		/// The body for the request.
+		/// </summary>
+		object Body { get; }
+
+		Type ContentAttributeType { get; }
+	}
+}
