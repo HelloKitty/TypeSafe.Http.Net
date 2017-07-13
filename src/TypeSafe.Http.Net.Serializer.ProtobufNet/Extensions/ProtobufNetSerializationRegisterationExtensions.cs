ed42c7146e5340a1d3a875a2a7609b1d85155ce3
HelloKitty <andrew.blakely@ymail.com>
2017-07-12 20:18:18 -0500

    Added Protobuf-Net Serialization Implementation

---
+++ src/TypeSafe.Http.Net.Serializer.ProtobufNet/Extensions/ProtobufNetSerializationRegisterationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeSafe.Http.Net
+{
+	public static class ProtobufNetSerializationRegisterationExtensions
+	{
+		/// <summary>
+		/// Registers the Protobuf-Net serializer in the serializer registeration service.
+		/// </summary>
+		/// <param name="builder">The builder being built.</param>
+		/// <returns>The builder for fluent chaining.</returns>
+		public static TSerializationRegisterationType RegisterProtobufNetSerializer<TSerializationRegisterationType>(this TSerializationRegisterationType builder)
+			where TSerializationRegisterationType : ISerializationStrategyRegister
+		{
+			builder.Register<ProtobufBodyAttribute, ProtobufNetProtobufBodySerializerStrategy>(new ProtobufNetProtobufBodySerializerStrategy());
+
+			//fluently return
+			return builder;
+		}
+	}
+}
