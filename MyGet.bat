f1d286ef89d90d516ca3e29ce15358e0f48fee87
HelloKitty <andrew.blakely@ymail.com>
2018-02-26 22:24:39 -0600

    Try to fix myget nuget restore

--- MyGet.bat
+++ MyGet.bat
@@ -1,2 +1,2 @@
-%NUGET% restore TypeSafe.HTTP.NET.sln -NoCache -NonInteractive -ConfigFile Nuget.config
+%NUGET% restore TypeSafe.HTTP.NET.sln -NoCache -NonInteractive -ConfigFile NuGet.config
 msbuild TypeSafe.HTTP.NET.sln /p:Configuration=Release
